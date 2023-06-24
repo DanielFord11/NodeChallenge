324e54323f23e4c6150131bcbf3317182f83401d
Daniel Ford <69986354+DanielFord11@users.noreply.github.com>
2023-06-24 13:17:22 -0500

    Update generateMarkdown.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,9 +1,22 @@
 // TODO: Create a function that returns the license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  // TODO: Implement logic to return the license badge based on the provided license
-  // Return an empty string if there is no license
-  return '';
+  let badge = '';
+  switch (license) {
+    case 'MIT':
+      badge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+      break;
+    case 'Apache-2.0':
+      badge = '[![License: Apache-2.0](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+      break;
+    case 'GPL-3.0':
+      badge = '[![License: GPL-3.0](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+      break;
+    default:
+      badge = '';
+      break;
+  }
+  return badge;
 }
 
 // TODO: Create a function that returns the license link
